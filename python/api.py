0af9a42dad570d8be3c8d5bc3e6b2310db31ff92
Shashi Shekhar <shashi.sh@Shashis-MacBook-Pro.local>
2021-04-11 00:32:42 +0530

    changes

--- python/api.py
+++ python/api.py
@@ -16,20 +16,18 @@ from_date = (to_date - datetime.timedelta(days=diff))
 # Interval(minute, day, 3 minute, 5 minute...)
 interval = "minute"
 
-#winddown calc. parameters
+# winddown calc. parameters
 tStart = '09:16:00'
 tEnd = '15:30:00'
 slidingWindow = 5
 min_winddown = 0.01
 
 def start(instrument_token, from_date, to_date, interval, tStart, tEnd, slidingWindow, min_winddown):
-
-    # Gets historical data from Kite Connect
+    #Gets historical data from Kite Connect
     def get_historical_data(instrument_token, from_date, to_date, interval):
 	    return kite.historical_data(instrument_token, from_date, to_date, interval)
 
     records = get_historical_data(instrument_token, from_date, to_date, interval)
-
     records_df = pd.DataFrame(records)
     #print(records_df.head())
 
